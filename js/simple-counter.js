ca28eaee90230522605388fbb198d6c12c36dd4d
TanzidAnan <tanzid.anan125@gmail.com>
2024-09-18 01:53:04 +0600

    add number

--- js/simple-counter.js
+++ js/simple-counter.js
@@ -1,1 +1,13 @@
-console.log(544)
+const count = document.getElementById('count');
+console.log(count.innerText);
+
+function increaseCount(){
+   let currentCount =Number(count.innerText);
+   currentCount++
+   count.innerText =currentCount;
+    
+}
+
+function discresCount(){
+    console.log(788)
+}
