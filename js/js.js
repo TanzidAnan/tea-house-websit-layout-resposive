9fd84b5ff05592eedeb29956867dd6e8bbfa2f6b
TanzidAnan <tanzid.anan125@gmail.com>
2024-09-18 01:39:27 +0600

    simple-count

--- js/js.js
+++ js/js.js
@@ -1,4 +1,11 @@
 const images =document.getElementsByTagName('img');
 const card =document.getElementsByClassName('grid');
-console.log(images)
-console.log(card)
+const button =document.getElementsByClassName('btnButton')[0]
+
+
+const tagName =document.getElementsByTagName('html')[0];
+tagName.setAttribute("data-theme","dark")
+console.log(tagName.getAttribute("data-theme"))
+// console.log(images)
+// console.log(card)
+console.log(button.textContent)
