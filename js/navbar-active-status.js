35bbedd9cc3756a4e6d39283250b2983b54f40f4
TanzidAnan <tanzid.anan125@gmail.com>
2024-09-18 10:01:36 +0600

    ul list

--- js/navbar-active-status.js
+++ js/navbar-active-status.js
@@ -1,10 +1,22 @@
-const ulTag =document.getElementById('nav-container');
-
-const listItem =ulTag.getElementsByTagName("li");
+const ulTag = document.getElementById('nav-container');
+let lastClick = null
+const listItem = ulTag.getElementsByTagName("li");
 // console.log(listItem);
 
-for(let signelListItem of listItem){
-    signelListItem.addEventListener('click',function(){
-        console.log(signelListItem.innerText)
+for (let signelListItem of listItem) {
+    signelListItem.addEventListener('click', function () {
+        if (lastClick== null) {
+            signelListItem.style.backgroundColor = "red"
+            signelListItem.style.color = 'white';
+            lastClick =signelListItem
+        }
+        else{
+            lastClick.style.backgroundColor ="transparent";
+            lastClick.style.color = '#eee';
+            signelListItem.style.backgroundColor ="red";
+            signelListItem.style.color ="white";
+            lastClick =signelListItem
+        }
+
     })
 }
